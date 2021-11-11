34fab22baf985f5aa4d28172c7ed58015f5fd129
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-11 00:35:24 +0000

    not much just wrote half the code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("user type number to add subtract multiply divide")
+print ("python program convert celcius temp to farenhit");
+print("accept the celcius temp from user");
+celcius = float(input("please enter the temp in celcius -"));
+farenheit = (celcius * 1.8)+32
+print('%0.1f degree celcius is equal to %0.1f degree farenheit '%(celcius,farenheit));
+temp = farenheit
+if(temp >= 105):
+  print ("it summer like weather")
+  elif (temp<=32):
+    print ('wow must be pretty cold there')
+    else:
+      print("temp is right between spring and fall not too cold not to warm in my thats the best temp")
